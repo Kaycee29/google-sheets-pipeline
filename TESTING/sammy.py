399b7fdb8446c0f3c999cd2a211ecce57a7307a1
Kaycee29 <kayceeojieh@gmail.com>
2025-06-14 16:15:31 +0100

    All done

--- TESTING/sammy.py
+++
@@ -1,8 +0,0 @@
-dag = DAG(
-    dag_id = "daily_gsheet_data_to_s3",
-    description = "This is my dag for daily weather extraction to s3",
-    start_date = datetime(2025,6,12),
-    schedule_interval = "0 12 * * *", # runs daily at 12pm
-    catchup= False,
-    default_args = default_args
-    )
